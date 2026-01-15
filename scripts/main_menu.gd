3ff061390c626c71319e6d401615b376d0e9eb4c
Ezpeasy0 <158174291+Ezpeasy0@users.noreply.github.com>
2026-01-15 21:32:56 +0700

    menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -17,7 +17,7 @@ func _process(delta: float) -> void:
 
 
 func _on_start_pressed() -> void:
-	pass #get_tree().change_scene_to_file()
+	get_tree().change_scene_to_file("res://scenes/stage.tscn")
 
 
 func _on_tutorial_pressed() -> void:
